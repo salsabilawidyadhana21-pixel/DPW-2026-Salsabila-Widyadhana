e81cf599a9065b8e682e3629049438bfeec74538
salsabilawidyadhana21 <salsabilawidyadhana21@gmail.com>
2026-09-14 14:33:44 +0700

    jb 7 : footer.php

---
+++ Jobsheet 7/includes/footer.php
@@ -0,0 +1,12 @@
+</main>
+
+<footer>
+    <p>
+        &copy; 2026 SIMPUS-Mini &mdash; Jobsheet 7
+    </p>
+</footer>
+
+<script src="<?php echo $base; ?>assets/js/app.js"></script>
+
+</body>
+</html>
